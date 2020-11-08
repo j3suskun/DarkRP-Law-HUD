0da7ab266e060850ce3b2a31829341749db41669
j3suskun <73049247+j3suskun@users.noreply.github.com>
2020-11-08 03:40:49 -0600

    documentation

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -1,24 +1,27 @@
+//creating fonts for later use
 surface.CreateFont("open_sans_25b", {font = "Open Sans Bold", size = 25, weight = 800, antialias = true})
 surface.CreateFont("open_sans_19b", {font = "Open Sans Bold", size = 19, weight = 800, antialias = true})
-surface.SetFont("open_sans_19b")
+surface.SetFont("open_sans_19b") //I'm not sure if this is necessary but it makes me feel more comfortable
 
+//literally the only color from the original code actually used
+//will probably get rid of it later
 local colour = {
-   //["pure_white"] = Color(255, 255, 255),
-   ["white"] = Color(220, 220, 220),
-   //["grey"] = Color(155, 155, 155),
-   //["darkest"] = Color(43, 49, 55),
-   //["dark"] = Color(55, 61, 67),
-   //["light"] = Color(101, 111, 123),
+   ["white"] = Color(220, 220, 220)
 }
 
-local showLawBox = true
+local showLawBox = true  //boolean value used to set whether box is shown
 
+/*
+This function is used to validate that the line number isn't too high
+Otherwise it could take up too much of a screen
+it returns the number of lines in the given text
+*/
 function GetLineNum(text)
   local lineNum = select(2, text:gsub('\n','\n'))
   return lineNum
 end
 
-function OpenLawsEditor()
+function OpenLawsEditor() //most of this function was from the original coder
   local Frame = vgui.Create( "DFrame" )
   Frame:SetSize( 600, 550 )
   Frame:SetTitle( "Laws Editor" )
@@ -27,10 +30,10 @@ function OpenLawsEditor()
   Frame:SetDraggable( true )
   Frame:ShowCloseButton( true )
   Frame:MakePopup()
-  Frame.Paint = function( self, w, h )
+  Frame.Paint = function( self, w, h ) //I don't like these variables but I'm not comfortable changing them
     draw.RoundedBox( 0, 0, 0, w, h, Color( 0, 0, 0, 200) )
   end
-
+//a lot of this you should go to facepunch wiki to figure out, not enough room to explain within code
   local TextEntry = vgui.Create( "DTextEntry", Frame )
   local lawTxt = laws or "Default laws."
   TextEntry:SetPos( 8, 25 )
@@ -43,8 +46,10 @@ function OpenLawsEditor()
   DermaButton:SetText( "Update Laws" )
   DermaButton:SetPos( 178 , 512  )
   DermaButton:SetSize( 250, 30 )
-  DermaButton.DoClick = function()
+  DermaButton.DoClick = function() //every time I try to seperate this function it breaks, so it stays here
     local lawValue = TextEntry:GetValue()
+    //if the line number or character number is too high I want to block the change
+    //this helps stop some amount of trolling
     if(GetLineNum(lawValue) > 14)
     then
       notification.AddLegacy("Exceeded line limit", 1, 3)
@@ -67,27 +72,28 @@ net.Receive( "LawsPublic", function()
   laws = net.ReadString()
 end)
 
-function DrawLawBox()
-  local lawTxt = laws or "Default laws."
-  local x = ScrW()
-  local y = ScrH()
+function DrawLawBox() //I think all of this function was from the original coder, so I can't explain much
+  local lawTxt = laws or "Default laws." //changing this breaks things
+  local x = ScrW() //width
+  local y = ScrH() //height
 
-  lawTxt = lawTxt:gsub("//", "\n"):gsub("\\n", "\n")
+  lawTxt = lawTxt:gsub("//", "\n"):gsub("\\n", "\n") //I'm not sure why this is here, but I'm not gonna fuck with it
   lawTxt = DarkRP.textWrap(lawTxt, "open_sans_19b", 445)
 
-  if (lawTxt == "") then lawTxt = "Default laws." end
+  if (lawTxt == "") then lawTxt = "Default laws." end //removing this breaks everything
 
   local txtWidth, txtHeight = surface.GetTextSize( lawTxt )
 
   draw.RoundedBox( 0, x * 0.76, y * 0.02, 455, txtHeight + 35, Color( 0, 0, 0, 128 ) )
   draw.DrawText( "Laws", "open_sans_25b", x * 0.766, y * 0.02, Color( 255, 255, 255, 255 ), TEXT_ALIGN_LEFT )
 
+//I'm not sure if all thre of the following draws are necessary
   draw.DrawNonParsedText(lawTxt, "open_sans_19b", x * 0.766, y * 0.043, Color(0, 0, 0, 170), 0)
   draw.DrawNonParsedText(lawTxt, "open_sans_19b", x * 0.766, y * 0.043, Color(0, 0, 0, 100), 0)
   draw.DrawNonParsedText(lawTxt, "open_sans_19b", x * 0.766, y * 0.043, colour.white, 0)
 end
 
-function lawBoxToggle()
+function lawBoxToggle() //toggles the law box for users
   if(showLawBox)
   then
     hook.Add( "HUDPaint", "HUDPaint_LawBox", DrawLawBox)
@@ -97,9 +103,9 @@ function lawBoxToggle()
     print("confirm false")//debug
   end
 end
-
+lawboxToggle()
 net.Receive("LawsToggle",function()
-  showLawBox = !showLawBox
+  showLawBox = !showLawBox //make the boolean value opposite of original value
   print("Lawbox value: ", showLawBox) //debug
   lawBoxToggle()
 end)
